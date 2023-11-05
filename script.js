13f1014be2ae26c1f14adce1cd400117142b160c
Romumoon <romu.pastore@gmail.com>
2023-11-05 00:22:37 -0300

    timer

--- script.js
+++ script.js
@@ -5,6 +5,27 @@ const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
+
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+
+const play = new Audio('/sons/play.wav')
+const pause = new Audio('/sons/pause.mp3')
+const beep = new Audio('/sons/beep.mp3')
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null
+
+musica.loop = true
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
@@ -44,4 +65,32 @@ function alterarContexto(contexto){
         default:
             break;
     }
+}
+
+const contagemReressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0){
+        zerar()
+        beep.play()
+        alert('Tempo finalizado!')
+        return
+    }
+    tempoDecorridoEmSegundos--
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if(intervaloId) {
+        pause.play()
+        zerar()
+        return
+    }
+    play.play()
+    intervaloId = setInterval(contagemReressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
 }
